a692469d894b901a1d8f41606308f7f0720de179
Güray Hatipoğlu <62484975+torna4o@users.noreply.github.com>
2023-11-06 15:41:41 +0300

    Update noisy.py
    Random strip generator added.

--- noisy.py
+++ noisy.py
@@ -1,9 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# Generate Gaussian noise
-
 def add_gauss(img, mean=0, std=1, speckle = False):
 
     """
@@ -42,46 +39,6 @@ def add_poisson(img, lam):
     
     return img_pois, pois
 
-def add_band_spec_noise(min_freq, max_freq, samples=1024, samplerate=1):
-    """
-    This code is mainly taken from this page:
-    https://stackoverflow.com/a/35091295/12656842
-    
-    The aim is creating a specific frequency band noise
-    via numpy.fft functions and several other numpy functions
-    
-    min_freq = minimum frequency of the specific noise
-    max_freq = maximum frequency of the specific noise
-    samples = effectively this is the size of the noise
-    samplerate = this adjusts the given frequency rate and resulting 
-        frequencies in the noise
-    
-    Example:
-    
-    import noisy as ns
-    # following produces a 512-length sinusoidal noise with 25 length cycles
-    ns.band_limited_noise(25,25, samples=512, samplerate=1/512)
-    
-    """
-    
-    # Generating a real noise from a DFT routine    
-    freqs = np.abs(np.fft.fftfreq(samples, 1/samplerate))
-    
-    f = np.zeros(samples)
-    # Locations of the array corresponding to the specified frequency range
-    idx = np.where(np.logical_and(freqs>=min_freq, freqs<=max_freq))[0]
-    f[idx] = 1
-    # Preparation to the phase addition to the array
-    f = np.array(f, dtype='complex')
-    Np = (len(f) - 1) // 2
-    phases = np.random.rand(Np) * 2 * np.pi
-    phases = np.cos(phases) + 1j * np.sin(phases)
-    f[1:Np+1] *= phases
-    # Remined, indexing works like [start:end:step]
-    f[-1:-1-Np:-1] = np.conj(f[1:Np+1])
-    return np.fft.ifft(f).real    
-    
-
 def saltpep(img, multi, threshold=0.99):
 
     """
@@ -93,8 +50,23 @@ def saltpep(img, multi, threshold=0.99):
     multi = this is a multiplier, in case one needs different numbers than 
         1 salt noise generation
     """
+    
     base = np.random.rand(img.shape[0], img.shape[1])
     base[base >= threshold] = 1
     base[base < threshold] = 0
     base *= multi
     return np.add(img, base)
+
+def rstrip(img, dire, s=1):
+    """
+    Creates single row (dire='h') or column (dire='v') of random numbers scaled
+    between 0 and s (default is s=1). Then, it broadcast the strips in the same shape as img
+    
+    img is a 2D array
+    dire is the direction of the random values strips to be generated
+    s is the right border of random number generator, np.random.rand
+    """
+    if dire == 'h':
+        return np.zeros((img.shape[0],img.shape[1]))+np.random.rand(img.shape[0],1)*s
+    elif dire == 'v':
+        return np.zeros((img.shape[0],img.shape[1]))+np.random.rand(1,img.shape[1])*s
